46733a629cacde3d55a32d829cbb30dbd237d6e2
van Hauser <vh@thc.org>
2020-12-18 10:21:43 +0100

    mem error fix

--- include/afl-fuzz.h
+++ include/afl-fuzz.h
@@ -1017,13 +1017,14 @@ u32  count_non_255_bytes(afl_state_t *, u8 *);
 void simplify_trace(afl_state_t *, u8 *);
 void init_count_class16(void);
 void classify_counts(afl_forkserver_t *fsrv);
-u8 has_new_bits_unclassified(afl_state_t *, u8 *);
 void minimize_bits(afl_state_t *, u8 *, u8 *);
+void discover_word(u8 *ret, u64 *current, u64 *virgin);
 #ifndef SIMPLE_FILES
 u8 *describe_op(afl_state_t *, u8, size_t);
 #endif
 u8 save_if_interesting(afl_state_t *, void *, u32, u8);
 u8 has_new_bits(afl_state_t *, u8 *);
+u8 has_new_bits_unclassified(afl_state_t *, u8 *);
 
 /* Extras */
 
